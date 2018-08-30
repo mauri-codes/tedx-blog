e6a2a60a6b8a871dc6482496484df14c31e84737
Mauricio Alarcon <mauri@localhost.localdomain>
2018-08-29 22:39:03 -0400

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'
@@ -7,6 +9,8 @@ import { AdminComponent } from './view/admin/admin.component'
 import { BlogComponent } from './view/blog/blog.component'
 
 import { QuillModule } from 'ngx-quill'
+
+let MaterialModules = [MatButtonModule, MatCheckboxModule]
 
 @NgModule({
   declarations: [
@@ -17,7 +21,9 @@ import { QuillModule } from 'ngx-quill'
   imports: [
     BrowserModule,
     QuillModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ... MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
